e18813aea2b379bc311459ddfe965ad2cec9211c
Eric <estraw@smu.edu>
2023-07-14 17:36:50 -0500

    Basic DB Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <h1>A layout thing</h1>
       <body className={inter.className}>{children}</body>
     </html>
   )
